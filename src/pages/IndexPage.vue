371d2e67b75fbdd80404380e0925caefae8c7e22
zed <1569060759@qq.com>
2022-06-07 15:05:30 +0800

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar()
+</script>
+
+<template>
+  <q-page padding>
+    <q-btn color="secondary" label="切换主题" @click='$q.dark.toggle'/>
+  </q-page>
+  <div class='bg-blue-1'>111</div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+}
+</style>
